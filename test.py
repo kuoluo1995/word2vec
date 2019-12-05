5281bd9ea75f100ffcb7712aa198224891734a58
kuoluo1995 <kuoluo1995@gmail.com>
2019-12-05 16:47:22 +0800

    updatread me

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import tensorflow as tf
 from utils import yaml_utils
 from sklearn.manifold import TSNE
 from matplotlib.font_manager import FontProperties
@@ -24,11 +25,37 @@ def visualization_result(final_embeddings_path, reverse_dictionary_path, output_
     plot_with_labels(low_dim_embs, labels, output_image, fonts=font)
 
 
+def most_similar(final_embeddings_path, reverse_dictionary_path, words):
+    final_embeddings = yaml_utils.read(final_embeddings_path)
+    reverse_dictionary = yaml_utils.read(reverse_dictionary_path)
+    tf.set_random_seed(19)
+    tf_config = tf.ConfigProto(allow_soft_placement=True)
+    tf_config.gpu_options.allow_growth = True
+    with tf.Session(config=tf_config) as sess:
+        valid_dataset = tf.placeholder(tf.int32, shape=[1])
+        normalized_embeddings = tf.placeholder(tf.float32, shape=[5000, 128])
+        valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings, valid_dataset)
+        similarity = tf.matmul(valid_embeddings, normalized_embeddings, transpose_b=True)
+        init_op = tf.global_variables_initializer()
+        sess.run(init_op)
+        sim = sess.run(similarity, feed_dict={valid_dataset: words, normalized_embeddings: final_embeddings})
+        valid_word = reverse_dictionary[words[0]]
+        top_k = 5
+        nearest = (-sim[0, :]).argsort()[1:top_k + 1]
+        log_str = '最接近 ' + valid_word + ' 的词语是:'
+        for k in range(top_k):
+            close_word = reverse_dictionary[nearest[k]]
+            log_str += str(k + 1) + ':' + close_word + ' '
+        print(log_str)
+
+
 if __name__ == '__main__':
-    dataset_name = 'text8'
+    dataset_name = 'wiki_corpus'
     model_name = 'SkipGram'
     tag = 'base'
     dataset_info = yaml_utils.read('dataset/' + dataset_name + '/info.yaml')
     checkpoint_dir = './_checkpoint/' + dataset_name + '/' + model_name + '/' + tag + '/'
-    visualization_result(checkpoint_dir + 'final_embeddings.yaml', dataset_info['reverse_dictionary'],
-                         checkpoint_dir + 'tsne.png', 100)
+    # visualization_result(checkpoint_dir + 'final_embeddings.yaml', dataset_info['reverse_dictionary'],
+    #                      checkpoint_dir + 'tsne.png', 100)
+
+    most_similar(checkpoint_dir + 'final_embeddings.yaml', dataset_info['reverse_dictionary'], [20])

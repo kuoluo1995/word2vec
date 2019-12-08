3b8e1c5fd467d38bccade9b78c5b2d6f99715321
kuoluo1995 <kuoluo1995@gmail.com>
2019-12-08 16:39:19 +0800

    update most_similar method

--- test.py
+++ test.py
@@ -26,31 +26,32 @@ def visualization_result(final_embeddings_path, reverse_dictionary_path, output_
 
 
 def most_similar(final_embeddings_path, reverse_dictionary_path, words):
-    final_embeddings = yaml_utils.read(final_embeddings_path)
     reverse_dictionary = yaml_utils.read(reverse_dictionary_path)
+    final_embeddings = yaml_utils.read(final_embeddings_path)
     tf.set_random_seed(19)
     tf_config = tf.ConfigProto(allow_soft_placement=True)
     tf_config.gpu_options.allow_growth = True
     with tf.Session(config=tf_config) as sess:
-        valid_dataset = tf.placeholder(tf.int32, shape=[1])
+        valid_dataset = tf.placeholder(tf.int32, shape=[5])
         normalized_embeddings = tf.placeholder(tf.float32, shape=[5000, 128])
         valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings, valid_dataset)
         similarity = tf.matmul(valid_embeddings, normalized_embeddings, transpose_b=True)
         init_op = tf.global_variables_initializer()
         sess.run(init_op)
         sim = sess.run(similarity, feed_dict={valid_dataset: words, normalized_embeddings: final_embeddings})
-        valid_word = reverse_dictionary[words[0]]
-        top_k = 5
-        nearest = (-sim[0, :]).argsort()[1:top_k + 1]
-        log_str = '最接近 ' + valid_word + ' 的词语是:'
-        for k in range(top_k):
-            close_word = reverse_dictionary[nearest[k]]
-            log_str += str(k + 1) + ':' + close_word + ' '
-        print(log_str)
+        for i in range(len(words)):
+            valid_word = reverse_dictionary[words[i]]
+            top_k = 5
+            nearest = (-sim[i, :]).argsort()[1:top_k + 1]
+            log_str = '最接近 ' + valid_word + ' 的词语是:'
+            for k in range(top_k):
+                close_word = reverse_dictionary[nearest[k]]
+                log_str += str(k + 1) + ':' + close_word + ' '
+            print(log_str)
 
 
 if __name__ == '__main__':
-    dataset_name = 'wiki_corpus'
+    dataset_name = 'text8'
     model_name = 'SkipGram'
     tag = 'base'
     dataset_info = yaml_utils.read('dataset/' + dataset_name + '/info.yaml')
@@ -58,4 +59,5 @@ if __name__ == '__main__':
     # visualization_result(checkpoint_dir + 'final_embeddings.yaml', dataset_info['reverse_dictionary'],
     #                      checkpoint_dir + 'tsne.png', 100)
 
-    most_similar(checkpoint_dir + 'final_embeddings.yaml', dataset_info['reverse_dictionary'], [62])
+    most_similar(checkpoint_dir + 'final_embeddings.yaml', dataset_info['reverse_dictionary'], [1, 8, 34, 96, 144])
+    # 62, 77, 95, 155, 187 wiki_corpus
